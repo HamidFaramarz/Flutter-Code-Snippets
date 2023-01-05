1921ced7ca7818810af1ae608e330c92f56b1be9
Hamid Faramarz <101446076+HamidFaramarz@users.noreply.github.com>
2023-01-05 23:48:58 +0430

    Create sizedBox.dart

---
+++ SizedBox/sizedBox.dart
@@ -0,0 +1,10 @@
+SizedBox(
+  width: 250,
+  height: 50,
+  child: OutlinedButton(
+    onPressed: () {
+      print("hello world");
+    },
+    child: const Text("English", style: ThemeText.btnA),
+  ),
+),
