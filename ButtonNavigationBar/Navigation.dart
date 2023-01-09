7d62513e1a4ddf8e7e3df83e8459b2660be7ddcc
Hamid Faramarz <101446076+HamidFaramarz@users.noreply.github.com>
2023-01-09 19:56:43 +0430

    Update Navigation.dart

--- ButtonNavigationBar/Navigation.dart
+++ ButtonNavigationBar/Navigation.dart
@@ -1,23 +1,5 @@
-class Employee {
-  final String name;
-  final String email;
-
-  Employee(this.name, this.email);
-
-  Employee.fromJson(Map<String, dynamic> json)
-      : name = json['name'],
-        email = json['email'];
-
-  Map<String, dynamic> toJson() =>
-    {
-      'name': name,
-      'email': email,
-    };
-}
-
-//Now CONVERT SIMPLE JSON TO FLUTTER OBJECT 
-Map employeeMap = jsonDecode(jsonString);
-var employee = Employee.fromJson(employeeMap);
-
-//CONVERT FLUTTER OBJECT TO SIMPLE JSON STRING
-String json = jsonEncode(employee);
+BottomNavigationBar(
+    showSelectedLabels: false,
+    showUnselectedLabels: false,
+    items: <BottomNavigationBarItem> []
+);
